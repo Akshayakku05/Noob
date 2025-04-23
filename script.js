fddcc504a7ccd57f2469c94428a5d3ce4bbad77d
Akshay E Muthu <akshayemuthu10a7@gmail.com>
2025-04-23 17:58:36 +0530

    nothing much

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('clickMe').addEventListener('click', () => {
+    const response = document.getElementById('response');
+    response.textContent = 'You clicked the button! 🎉';
+  });
+  
